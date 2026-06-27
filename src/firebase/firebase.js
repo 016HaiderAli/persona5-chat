9eb270771a715d10ce74d07b098bec300f805a8c
Haider <haiderali_zaidi@outlook.com>
2026-06-27 06:11:33 +0500

    Added env config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,18 +2,35 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA7T19Xt5GpOf54cLWxSzgSm8fmxzS6yuw",
-  authDomain: "persona5-chat-dcea8.firebaseapp.com",
-  projectId: "persona5-chat-dcea8",
-  storageBucket: "persona5-chat-dcea8.firebasestorage.app",
-  messagingSenderId: "647602122930",
-  appId: "1:647602122930:web:c12cf58b36a5fd29ec99ab"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA7T19Xt5GpOf54cLWxSzgSm8fmxzS6yuw",
+//   authDomain: "persona5-chat-dcea8.firebaseapp.com",
+//   projectId: "persona5-chat-dcea8",
+//   storageBucket: "persona5-chat-dcea8.firebasestorage.app",
+//   messagingSenderId: "647602122930",
+//   appId: "1:647602122930:web:c12cf58b36a5fd29ec99ab"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// export const auth = getAuth(app);
+// export const db = getFirestore(app);
